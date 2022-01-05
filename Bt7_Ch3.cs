359cabbedf048de11e9a2f8178d04b9b1e711940
muaxualo123 <hung01645167156@gmail.com>
2022-01-05 11:20:43 +0700

    Create Bt7_Ch3.cs

---
+++ Bt7_Ch3.cs
@@ -0,0 +1,30 @@
+﻿/*
+7.	Viết chương trình nhập vào số giây từ 0 đến 86399, đổi số giây nhập vào thành dạng "gio:phut:giay", mỗi thành phần là một số nguyên có 2 chữ số.
+tên : đỗ phan duy hùng
+lớp cd21tt11
+*/
+using System;
+
+namespace bt7_ch3
+{
+    class Bt7_Ch3
+    {
+        static void Main(string[] args)
+        {
+            int giay = 0;
+            int gio = 0;
+            int phut = 0;
+            int sogiaynhap = 0;
+            Console.Write("nhap so giay = ");
+          sogiaynhap = Convert.ToInt32(Console.ReadLine());
+            gio = sogiaynhap / 3600;
+            phut = (sogiaynhap % 3600) / 60;
+            giay = sogiaynhap%60;
+
+            Console.WriteLine($"{gio:00} : {phut:00}:{giay:00}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
